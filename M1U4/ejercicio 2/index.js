588a5efc8de773ead0b51d226f74952c5e2381b0
Floremcia Carranza <florenciacarranza97@gmail.com>
2023-04-27 13:28:42 -0300

    M1U4

--- M1U4/ejercicio 2/index.js
+++ M1U4/ejercicio 2/index.js
@@ -3,7 +3,7 @@
 Haciendo uso de los bucles, recorrer un array de números y determinar cual es el mayor */
 
 
-const num =[100, 0, 2000, 500, 354770, 1000,];
+const num =[100, 0, 2000, 500, 354770, 1000];
 let cont=0;
 let numMayor=0; 
 
